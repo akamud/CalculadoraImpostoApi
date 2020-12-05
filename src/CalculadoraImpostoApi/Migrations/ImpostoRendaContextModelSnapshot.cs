1ef827b387108e05a286bedd81c7c79298f89f58
Mahmoud Ali <muddibr@gmail.com>
2020-12-05 19:18:25 -0300

    WIP

--- src/CalculadoraImpostoApi/Migrations/ImpostoRendaContextModelSnapshot.cs
+++ src/CalculadoraImpostoApi/Migrations/ImpostoRendaContextModelSnapshot.cs
@@ -2,17 +2,15 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoSample.Dados;
 
 namespace ToDoSample.Migrations
 {
     [DbContext(typeof(ImpostoRendaContext))]
-    [Migration("20201201022553_MigrationInicial")]
-    partial class MigrationInicial
+    partial class ImpostoRendaContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -20,7 +18,7 @@ namespace ToDoSample.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("ToDoSample.Dados.HistoricoCalculo", b =>
+            modelBuilder.Entity("CalculadoraImpostoApi.Dados.HistoricoCalculo", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
